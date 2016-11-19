7e949f65e3bfc02a261c3eda31b8c392be131af5
Mrucznik <mrucznix@gmail.com>
2016-11-19 12:43:16 +0100

    Add project files.

---
+++ Gwint/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Gwint
+{
+    [Activity(Label = "Gwint", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
