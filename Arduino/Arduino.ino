561d6fa7064a5cf4d9ac9ec813a7f0d6c32a1173
Mrucznik <mrucznix@gmail.com>
2016-11-19 13:03:40 +0100

    Arduino project (dodałem używając dodatku do VS http://www.visualmicro.com/)

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino.ino
+ Created:	11/19/2016 1:02:29 PM
+ Author:	Mrucznik
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
